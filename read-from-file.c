558e062f581b56ce730cca7fe2f091180764da82
LkkLuke <57714651+LkkLuke@users.noreply.github.com>
2020-01-02 11:51:08 +0800

    Add files via upload

--- read-from-file.c
+++ read-from-file.c
@@ -7,6 +7,8 @@
 #include <time.h>
 
 #include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 #define MAC_ADDRSTRLEN 2*6+5+1
 #define STR_BUF 16
@@ -14,8 +16,11 @@ static const char *mac_ntoa(u_int8_t *d);
 static const char *ip_ntoa(void *i);
 static const char *ip_ttoa(u_int8_t flag);
 static const char *ip_ftoa(u_int16_t flag);
+static const char *tcp_ftoa(u_int8_t flag);
 static void dump_ethernet(u_int32_t length, const u_char *content);
 static void dump_ip(u_int32_t length, const u_char *content);
+static void dump_tcp(u_int32_t length, const u_char *content);
+static void dump_udp(u_int32_t length, const u_char *content);
 static void pcap_callback(u_char *arg, const struct pcap_pkthdr *header, const u_char *content);
     int total_amount = 0;
     int total_bytes = 0;
@@ -108,16 +113,16 @@ static void dump_ethernet(u_int32_t length, const u_char *content) {
     else
         printf("Ethernet Frame:\n");
     
-    printf("+-------------------------+-------------------------+-------------------------+\n");
-    printf("| Destination MAC Address:                                   %17s|\n", dst_mac);
-    printf("+-------------------------+-------------------------+-------------------------+\n");
-    printf("| Source MAC Address:                                        %17s|\n", src_mac);
-    printf("+-------------------------+-------------------------+-------------------------+\n");
+    //printf("+-------------------------+-------------------------+-------------------------+\n");
+    printf("Destination MAC Address:         %17s\n", dst_mac);
+    //printf("+-------------------------+-------------------------+-------------------------+\n");
+    printf("Source MAC Address:              %17s\n", src_mac);
+    //printf("+-------------------------+-------------------------+-------------------------+\n");
     if (type < 1500)
-        printf("| Length:            %5u|\n", type);
+        printf("Length:            %5u|\n", type);
     else
-        printf("| Ethernet Type:    0x%04x|\n", type);
-    printf("+-------------------------+\n");
+        printf("Ethernet Type:    0x%04x|\n", type);
+    //printf("+-------------------------+\n");
     
     printf("Next protocol is ");
     switch (type) {
@@ -193,6 +198,27 @@ static const char *ip_ftoa(u_int16_t flag) {
     
     return str;
 }//end ip_ftoa
+
+static const char *tcp_ftoa(u_int8_t flag) {
+    static int  f[] = {'W', 'E', 'U', 'A', 'P', 'R', 'S', 'F'};
+#define TCP_FLG_MAX (sizeof f / sizeof f[0])
+    static char str[TCP_FLG_MAX + 1];
+    u_int32_t mask = 1 << 7;
+    int i;
+    
+    for (i = 0; i < TCP_FLG_MAX; i++) {
+        if(mask & flag)
+            str[i] = f[i];
+        else
+            str[i] = '-';
+        mask >>= 1;
+    }//end for
+    str[i] = '\0';
+    
+    return str;
+}//end tcp_ftoa
+
+
 static void dump_ip(u_int32_t length, const u_char *content) {
     struct ip *ip = (struct ip *)(content + ETHER_HDR_LEN);
     u_int version = ip->ip_v;
@@ -213,28 +239,27 @@ static void dump_ip(u_int32_t length, const u_char *content) {
     
     //print
     printf("Protocol: IP\n");
-    printf("+-----+------+------------+-------------------------+\n");
-    printf("| IV:%1u| HL:%2u| T: %8s| Total Length: %10u|\n",
-           version, header_len, ip_ttoa(tos), total_len);
-    printf("+-----+------+------------+-------+-----------------+\n");
-    printf("| Identifier:        %5u| FF:%3s| FO:        %5u|\n",
-           id, ip_ftoa(offset), offset & IP_OFFMASK);
-    printf("+------------+------------+-------+-----------------+\n");
-    printf("| TTL:    %3u| Pro:    %3u| Header Checksum:  %#06x|\n",
-           ttl, protocol, checksum);
-    printf("+------------+------------+-------------------------+\n");
-    printf("| Source IP Address:                 %15s|\n", src_ip);
-    printf("+---------------------------------------------------+\n");
-    printf("| Destination IP Address:            %15s|\n", dst_ip);
-    printf("+---------------------------------------------------+\n");
+    //printf("+-----+------+------------+-------------------------+\n");
+    //printf("IV:%1u| HL:%2u| T: %8s| Total Length: %10u\n",version, header_len, ip_ttoa(tos), total_len);
+    //printf("+-----+------+------------+-------+-----------------+\n");
+    //printf("Identifier:        %5u| FF:%3s| FO:        %5u\n",id, ip_ftoa(offset), offset & IP_OFFMASK);
+    //printf("+------------+------------+-------+-----------------+\n");
+    //printf("TTL:    %3u| Pro:    %3u| Header Checksum:  %#06x\n",ttl, protocol, checksum);
+    //printf("+------------+------------+-------------------------+\n");
+    printf("Source IP Address:                 %15s\n", src_ip);
+    //printf("+---------------------------------------------------+\n");
+    printf("Destination IP Address:            %15s\n", dst_ip);
+    //printf("+---------------------------------------------------+\n");
     
     switch (protocol) {
         case IPPROTO_UDP:
             printf("Next is UDP\n");
+			dump_udp(length, content);
             break;
             
         case IPPROTO_TCP:
             printf("Next is TCP\n");
+			dump_tcp(length, content);
             break;
             
         case IPPROTO_ICMP:
@@ -246,3 +271,49 @@ static void dump_ip(u_int32_t length, const u_char *content) {
             break;
     }//end switch
 }//end dump_ip
+static void dump_tcp(u_int32_t length, const u_char *content) {
+    struct ip *ip = (struct ip *)(content + ETHER_HDR_LEN);
+    struct tcphdr *tcp = (struct tcphdr *)(content + ETHER_HDR_LEN + (ip->ip_hl << 2));
+    
+    //copy header
+    u_int16_t source_port = ntohs(tcp->th_sport);
+    u_int16_t destination_port = ntohs(tcp->th_dport);
+    u_int32_t sequence = ntohl(tcp->th_seq);
+    u_int32_t ack = ntohl(tcp->th_ack);
+    u_int8_t header_len = tcp->th_off << 2;
+    u_int8_t flags = tcp->th_flags;
+    u_int16_t window = ntohs(tcp->th_win);
+    u_int16_t checksum = ntohs(tcp->th_sum);
+    u_int16_t urgent = ntohs(tcp->th_urp);
+    
+    //print
+    printf("Protocol: TCP\n");
+    //printf("+-------------------------+-------------------------+\n");
+    printf("Source Port:       %5u| Destination Port:  %5u\n", source_port, destination_port);
+    //printf("+-------------------------+-------------------------+\n");
+    //printf("Sequence Number:                        %10u\n", sequence);
+    //printf("+---------------------------------------------------+\n");
+    //printf("Acknowledgement Number:                 %10u\n", ack);
+    //printf("+------+-------+----------+-------------------------+\n");
+    //printf("HL:%2u|  RSV  |F:%8s| Window Size:       %5u\n", header_len, tcp_ftoa(flags), window);
+    //printf("+------+-------+----------+-------------------------+\n");
+    //printf("Checksum:          %5u| Urgent Pointer:    %5u\n", checksum, urgent);
+    //printf("+-------------------------+-------------------------+\n");
+}//end dump_tcp
+
+static void dump_udp(u_int32_t length, const u_char *content) {
+    struct ip *ip = (struct ip *)(content + ETHER_HDR_LEN);
+    struct udphdr *udp = (struct udphdr *)(content + ETHER_HDR_LEN + (ip->ip_hl << 2));
+    
+    u_int16_t source_port = ntohs(udp->uh_sport);
+    u_int16_t destination_port = ntohs(udp->uh_dport);
+    u_int16_t len = ntohs(udp->uh_ulen);
+    u_int16_t checksum = ntohs(udp->uh_sum);
+    
+    printf("Protocol: UDP\n");
+    //printf("+-------------------------+-------------------------+\n");
+    printf("Source Port:       %5u| Destination Port:  %5u\n", source_port, destination_port);
+    //printf("+-------------------------+-------------------------+\n");
+    printf("Length:            %5u| Checksum:          %5u\n", len, checksum);
+    //printf("+-------------------------+-------------------------+\n");
+}//end dump_udp
